f71b1102a5abfa4ceb6f69b8daf0d8b296b9adc1
capt-ines <ines.swieczkowska02@gmail.com>
2025-12-17 18:57:27 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { Hero } from "@/components/sections/HeroSection";
 import { fetchMainPageData } from "@/lib/contentful";
 
 export const revalidate = false;
+// TODO: 1. stworzyć schema dla strony glownej 2. install hygraph i libki np. rich text editor.. 3. stworzyć fetcher  hygraph sdk  4. codegen 5. stworzyc nowy branch git checkout nazwa hygraph 6. gqltada
 
 export default async function Home() {
   const mainPageData = await fetchMainPageData();
